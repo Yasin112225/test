cda039d8e4d29856457cd57b4fbd4805bf7a50c3
yasin112225 <117225039+Yasin112225@users.noreply.github.com>
2025-02-07 21:13:17 +0330

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', function() {
+    // مدت زمان لودر (مثلاً 3 ثانیه)
+    var loaderTime = 1000; // 3000 میلی‌ثانیه معادل 3 ثانیه
+  
+    // بعد از زمان مشخص شده، لودر را مخفی کن
+    setTimeout(function() {
+      document.getElementById('loader').style.display = 'none';
+      document.getElementById('content').style.display = 'block';
+    }, loaderTime);
+  });
+  
